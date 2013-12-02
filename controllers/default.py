f34ca76001b07db8ba1bdf123de1eac53591ce4f
jeanpaulsc <jeanpaulsc@gmail.com>
2013-12-01 17:52:09 -0800

    block commit

---
+++ controllers/default.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# {{=response.toolbar()}}
+
+def index():
+    '''This is intended to display problems in groups as chapters'''
+    problems = db().select(db.problem.id,db.problem.title,orderby=db.problem.chapter)
+    response.flash = T("Prepare to Curse!")
+    return dict(problems=problems)
+
+@auth.requires_login()
+def add():
+    '''problems are entered here'''
+    form = SQLFORM(db.problem).process(next=URL('show'))
+    return dict(form=form)
+
+@auth.requires_login()
+def show():
+    problem = db.problem(request.args(0,cast=int)) #or redirect(URL('index'))
+    db.progress.problem_id.default = problem.id
+    form = SQLFORM(db.progress)
+    if form.process().accepted:
+        response.flash = 'your comment is posted'
+    comments = db(db.progress.problem_id==problem.id).select()
+    return dict(problem=problem, comments=comments, form=form)
+
+def download():
+    return response.download(request,db)
+
+def link():
+    return response.download(request,db,attachment=False)
+'''
+def index():
+    image_form = FORM(
+        INPUT(_name='image_title',_type='text'),
+        INPUT(_name='image_file',_type='file')
+        )
+
+    if image_form.accepts(request.vars,formname='image_form'):
+        image = db.image.file.store(image_form.vars.image_file.file, image_form.vars.image_file.filename)
+        id = db.image.insert(file=image,title=image_form.vars.image_title)
+        images = db().select(db.image.ALL)
+        return dict(images=images)
+'''
+@auth.requires_login()
+def comment():
+    '''either insight or criticism, it's done here'''
+    form = SQLFORM(db.progress).process(next=URL('index'))
+    return dict(form=form)
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
+
+
+@auth.requires_signature()
+def data():
+    """
+    http://..../[app]/default/data/tables
+    http://..../[app]/default/data/create/[table]
+    http://..../[app]/default/data/read/[table]/[id]
+    http://..../[app]/default/data/update/[table]/[id]
+    http://..../[app]/default/data/delete/[table]/[id]
+    http://..../[app]/default/data/select/[table]
+    http://..../[app]/default/data/search/[table]
+    but URLs must be signed, i.e. linked with
+      A('table',_href=URL('data/tables',user_signature=True))
+    or with the signed load operator
+      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
+    """
+    return dict(form=crud())
---
+++ models/snode.py
@@ -0,0 +1,80 @@
+# coding: utf8
+#db = DAL('sqlite://storage.sqlite',pool_size=1)
+db = DAL("sqlite://storage.sqlite")
+'''
+this table functions to retain both the problem text, likely 
+accompanied by an image directly from the book, hand-out etc.
+'''
+db.define_table('problem',
+    Field('title'),
+    Field('chapter', 'integer'),
+    Field('problem', 'integer'),
+    Field('problem_image', 'upload'),
+    Field('body', 'text'),
+    Field('solution_image', 'upload'),
+    Field('solution'),
+    Field('status'),
+    Field('origin'),
+    Field('comments', 'text'),
+    format = '%(problem)s')
+
+#'''progress refers both to proposals and critiques'''
+db.define_table('progress',
+<<<<<<< HEAD
+    Field('problem_id', 'reference problem'),
+    Field('perspective'),
+    Field('body', 'text'),
+    Field('comments', 'text'),
+    Field('created_by'),
+    Field('created_on'))
+
+'''this is my timer, reminder and monitor'''
+db.define_table('crontime',
+    Field('crontab'),
+    format = '%(crontime)s')
+
+db.define_table('snode',
+    Field('parent_id', 'reference self'),
+    Field('root', 'integer'),
+    Field('title'),
+    Field('body', 'text'))
+
+db.define_table('image',
+    Field('title'),
+    Field('file', 'upload'),
+    format = '%(title)s')
+=======
+                Field('problem_id', 'integer', 'reference problem'),
+                Field('perspective'),
+                Field('body', 'text'),
+                Field('comments', 'text'),
+		Field('created_by'),
+		Field('created_on'))
+
+'''this is my timer, reminder and monitor'''
+db.define_table('crontime',
+		Field('crontab'),
+                format = '%(crontime)s')
+
+db.define_table('snode',
+		Field('parent_id', 'reference self'),
+		Field('root', 'integer'),
+		Field('title'),
+		Field('body', 'text'))
+>>>>>>> 27a90a64303964c62128e728bdcf386beb4ca6fe
+
+db.problem.title.requires = IS_NOT_IN_DB(db, db.problem.title)
+db.progress.problem_id.requires = IS_IN_DB(db, db.problem.id, '%(title)s')
+db.problem.title.requires = IS_NOT_IN_DB(db, 'problem.title')
+db.problem.body.requires = IS_NOT_EMPTY()
+db.problem.solution.readable = db.problem.solution.writable = False
+##db.problem.created_on.readable = db.page.created_on.writable = False
+
+db.progress.body.requires = IS_NOT_EMPTY()
+db.progress.problem_id.readable = db.progress.problem_id.writable = False
+db.progress.created_by.readable = db.progress.created_by.writable = False
+db.progress.created_on.readable = db.progress.created_on.writable = False
+
+from gluon.tools import Auth
+auth = Auth(db)
+auth.define_tables(username=True)
